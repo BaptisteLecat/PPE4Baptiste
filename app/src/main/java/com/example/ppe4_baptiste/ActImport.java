8fbcc09407d646659ab546e1a2cec1dbc32e589c
Baptiste <baptiste.lecat44@gmail.com>
2021-04-16 13:40:52 +0200

    Debut TP 3.4

---
+++ app/src/main/java/com/example/ppe4_baptiste/ActImport.java
@@ -0,0 +1,14 @@
+package com.example.ppe4_baptiste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActImport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_import);
+    }
+}
